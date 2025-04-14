d79ae32ede64e5a88604fadc9e9ef6b3e123daf0
Saba-Burduli <sabagg790@gmail.com>
2025-04-14 17:34:16 +0400

    implemented student feature

---
+++ ConsoleApp1/Student.cs
@@ -0,0 +1,17 @@
+﻿namespace ConsoleApp1;
+
+public class Student
+{
+    public string Name { get; set; }
+    public string LastName { get; set; }    
+    public Student(string name, string lastName)
+    {
+        Name = name;
+        LastName = lastName;
+    }
+
+    public override string ToString()
+    {
+        return $"{Name} {LastName}";
+    }
+}
