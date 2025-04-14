41d17d330a0a8726c032f377ddcbb81ea0ed80fd
Saba-Burduli <sabagg790@gmail.com>
2025-04-14 17:43:54 +0400

    implemented age, added parameter to Student constructor

--- ConsoleApp1/Student.cs
+++ ConsoleApp1/Student.cs
@@ -2,12 +2,15 @@
 
 public class Student
 {
-    public string Name { get; set; }
-    public string LastName { get; set; }    
-    public Student(string name, string lastName)
+    public string Name { get; }
+    public string LastName { get; }
+    public int Age { get; set; }
+
+    public Student(string name, string lastName, int age)
     {
         Name = name;
         LastName = lastName;
+        Age = age;
     }
 
     public override string ToString()
